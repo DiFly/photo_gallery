14c6b3596f47b68d7d024f2af134ef608a68ac96
difly <kryshen.yaroslav@gmail.com>
2020-08-24 15:41:25 +0300

    add header style, change index.js

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+function sidebarOpen() {
+  console.log("Sidebar OPENED");
+  document.getElementById('sidebar').style.display = "block";
+}
+
+function sidebarClose() {
+  console.log("Sidebar CLOSED");
+}
